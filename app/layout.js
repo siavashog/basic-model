f02afced42d8de881bd5128c0fafb07ebc8212e1
siavash maghsoudi <siavashmaghsoudi@siavashs-MacBook-Air.local>
2023-06-10 09:28:14 +0330

    update 1

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import AuthProvider from "./components/AuthProvider/AuthProvider";
+import {ThemeProvider} from "./context/ThemeContext";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+      <ThemeProvider>
+          <AuthProvider>
+            <div className="container">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </AuthProvider>
+        </ThemeProvider>
+
+      </body>
     </html>
   )
 }
